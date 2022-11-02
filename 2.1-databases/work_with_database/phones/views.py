6cebef7d4ca446b8223446bab760a17979397694
password <moto198720@yandex.ru>
2022-11-02 21:50:36 +0300

    ДЗ 2.1

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +7,24 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort', '')
+    if sort == 'name':
+        phones = Phone.objects.order_by("name")
+        context = {'phones': phones}
+    elif sort == 'min_price':
+        phones = Phone.objects.order_by("price")
+        context = {'phones': phones}
+    elif sort == 'max_price':
+        phones = Phone.objects.order_by("-price")
+        context = {'phones': phones}
+    else:
+        phones = Phone.objects.all()
+        context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phones = Phone.objects.get(slug=slug)
+    context = {'phones': phones}
     return render(request, template, context)
