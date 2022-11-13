30de23f3b320fea5de743f4e134bf28def5d361a
password <moto198720@yandex.ru>
2022-11-13 19:46:07 +0300

    ДЗ 2.2 (задача 2)

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -14,3 +14,25 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+
+    name = models.CharField(max_length=256, verbose_name='Раздел')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleTag(models.Model):
+
+    article = models.ForeignKey('Article', verbose_name='Статья', on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey('Tag', verbose_name='Раздел', on_delete=models.CASCADE, related_name='articles')
+    is_main = models.BooleanField(default=False)
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
